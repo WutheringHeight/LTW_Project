b69891e887af998d31bf584adcd3056c48f8f3f4
WutheringHeights <23130289@st.hcmuaf.edu.vn>
2025-11-23 22:26:55 +0700

    second to last finish up

--- project/UserDetails/script.js
+++ project/UserDetails/script.js
@@ -2,8 +2,9 @@ function toggleDisplay(id){
     const e = document.getElementById(id)
     e.classList.toggle("non-display")
 }
-function makeNonDisplay(ids){
-    for(let id in ids){
+function makeNonDisplay(...ids){
+    for(let id of ids){
+        console.log(id)
         const e = document.getElementById(id)
         e.classList.add("non-display")
     } 
